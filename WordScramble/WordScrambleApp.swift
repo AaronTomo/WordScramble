64c659a323239cfc9d9cbf9d5cbe688563f372d5
Aaron Thompson <aaronseanthompson@icloud.com>
2024-06-30 15:20:11 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Aaron Thompson on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
